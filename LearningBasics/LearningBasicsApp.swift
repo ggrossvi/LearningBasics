12002860078bc0e18e3a9700a27dc3f0f2e919e7
Gloria Villa <hello.gloria@gmail.com>
2024-02-29 21:57:46 -0800

    Initial Commit

---
+++ LearningBasics/LearningBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningBasicsApp.swift
+//  LearningBasics
+//
+//  Created by Gloria Villa on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
